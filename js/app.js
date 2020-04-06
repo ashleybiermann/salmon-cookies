08ead541e00a7f9a02269daacf601497baa92684
Ashley <ashleybiermann@gmail.com>
2020-04-06 14:20:54 -0700

    outlined task in JS app

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
 document.write('HellO!');
+
+// Task: Create object literals for each shop LOCATION that outputs the following:
+// 1. Min Customers per Hour
+// 2. Max Customers per Hour
+// 3. Average Cookies Purchased per Customer
+// 4. Method to generate random number of customers per hour (with Min and Max) - Objects/Math/random
+// 5. For each Hour of Operation (6:00AM to 8:00PM), calculate and store the amount of Cookies Purchased - Use 3. and 4. || Store results in an array. Make the array be a property of the Location object it represents
+
+// Display the values of each array as an UnOrderedLIst in the browser
+// Calculate the Sum of these Hourly Totals, Display it, too
