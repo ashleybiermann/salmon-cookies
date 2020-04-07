8516764670809bf552c90fe0895b852b343082f3
Ashley <ashleybiermann@gmail.com>
2020-04-06 19:20:23 -0700

    writes out hoursOpen to document on single line

--- js/app.js
+++ js/app.js
@@ -1,7 +1,5 @@
 'use strict';
 
-document.write('HellO!');
-
 // Task: Create object literals for each shop LOCATION that outputs the following:
 // 1. Min Customers per Hour
 // 2. Max Customers per Hour
@@ -11,6 +9,7 @@ var seattleLocation = {
   minHourlyCustomers : 23,
   maxHourlyCustomers : 65,
   avgCookiesPerCustomer : 6.3,
+  hoursOpen : ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'],
   // 4. Method to generate random number of customers per hour (with Min and Max) - Objects/Math/random
   calculateRandNumOfCust : function() {
     var min = this.minHourlyCustomers;
@@ -19,11 +18,9 @@ var seattleLocation = {
   },
 
   // define another method that uses a for loop and calls calculateRandNumOfCust
-  hoursOpen : ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'],
-
   calculateNumCustPerHour : function(){
     // -put into an array from there // var numCustPerHour = []
-    
+    //TODO: How do I extract values from this array to use?
     var numCustPerHour = [];
     for(var i = 0; i < this.hoursOpen.length; i++) {
       numCustPerHour.push(this.calculateRandNumOfCust());
@@ -32,10 +29,14 @@ var seattleLocation = {
   },
 };
 
-console.log(seattleLocation);
-
 
 // 5. For each Hour of Operation (6:00AM to 8:00PM all stores), calculate and store the amount of Cookies Purchased - Use 3. and 4. || Store results in an array. Make the array be a property of the Location object it represents
 
 // Display the values of each array as an UnOrderedLIst in the browser
+  //currently able to display the hoursOpen array
+  //TODO: also show the numCustPerHour array
+for(var i = 0; i < seattleLocation.hoursOpen.length; i++) {
+  document.write(seattleLocation.hoursOpen[i]);
+}
+
 // Calculate the Sum of these Hourly Totals, Display it, too
